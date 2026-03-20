3e08a40ce6e44d99071497bf26b066dd6e501bc5
Karel Tropp <koder@kareltropp.cz>
2026-03-20 12:18:08 +0100

    dashboard mockup

--- src/apps/admin/app/page.tsx
+++ src/apps/admin/app/page.tsx
@@ -11,7 +11,81 @@ export async function generateMetadata() {
 
 export default function Home() {
     const t = useTranslations('dashboard');
-    return (
+    return <>
         <h1 className="text-4xl font-bold">{t('title')}</h1>
-    );
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+            <div className="flex flex-col gap-4">
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+                <div className="border border-laureo-border dark:border-laureo-border-dark rounded-lg">
+                    <div className="p-4 border-b border-laureo-border dark:border-laureo-border-dark">
+                        <h2 className="text-lg font-bold">Title</h2>
+                    </div>
+                    <div className="p-4">
+                        Todo: content
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>;
 }
